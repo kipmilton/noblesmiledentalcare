3950c40b34a658e3716708958d15e54c70b58d81
kipmilton <kipmilton71@gmail.com>
2025-02-08 13:24:31 +0300

    my final commit

--- myproj_dental/settings.py
+++ myproj_dental/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-zs9f%-m52_#7unr8xx25b!3127l0ckn&ku1i%t(eglte4000el
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 # Application definition
 
